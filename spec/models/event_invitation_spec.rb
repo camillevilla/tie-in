993e2a847efcb9c55bea75aab6c5f799f3e237c6
Camille Villa <cvilla100@gmail.com>
2016-09-24 11:26:07 -0700

    Add model test for EventInvitation

--- spec/models/event_invitation_spec.rb
+++ spec/models/event_invitation_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe EventInvitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it { should have_db_column(:sender_id) }
+    it { should have_db_column(:recipient_id) }
+    it { should have_db_column(:event_id) }
+    it { should have_db_column(:accepted) }
+  end
 end
