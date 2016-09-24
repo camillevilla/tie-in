15c66ec024e68c419b763f442741bfe2880e405b
Camille Villa <cvilla100@gmail.com>
2016-09-24 14:44:30 -0700

    Rename associations between User and EventInvitation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :trips
   has_many :created_trips, foreign_key: "creator_id", class_name: "Trip"
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
-  has_many :sent_invitations, foreign_key: "sender_id", class_name: "EventInvitation"
-  has_many :received_invitations, foreign_key: "recipient_id", class_name: "EventInvitation"
+  has_many :sent_event_invitations, foreign_key: "sender_id", class_name: "EventInvitation"
+  has_many :received_event_invitations, foreign_key: "recipient_id", class_name: "EventInvitation"
 end
