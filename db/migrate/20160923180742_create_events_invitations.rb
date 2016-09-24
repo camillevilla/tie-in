5003c62f29d3dd19b72fe7f6baca4e0684b6e44b
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-09-24 10:49:33 -0700

    Finish migrations

--- db/migrate/20160923180742_create_events_invitations.rb
+++
@@ -1,6 +0,0 @@
-class CreateEventsInvitations < ActiveRecord::Migration[5.0]
-  def change
-    create_table :events_invitations do |t|
-    end
-  end
-end
--- db/migrate/20160923180832_create_invitations_trips.rb
+++ db/migrate/20160923180832_create_invitations_trips.rb
@@ -1,6 +1,10 @@
 class CreateInvitationsTrips < ActiveRecord::Migration[5.0]
   def change
     create_table :invitations_trips do |t|
+      t.integer :sender_id
+      t.integer :recipient_id
+      t.integer :trip_id
+      t.boolean :accepted
     end
   end
 end
