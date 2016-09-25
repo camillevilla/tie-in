5900ec7688e761105141960cf6e6c2ce71957a91
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-09-25 15:33:09 -0700

    Add seeds for trips and events

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+def select_id(users)
+  users.sample.id
+end
+
 User.destroy_all
+Trip.destroy_all
+Event.destroy_all
 
 10.times do
-User.create(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  email: Faker::Internet.email,
-  password: Faker::Internet.password
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+@users = User.all
+
+10.times do
+  Trip.create(
+    creator_id: @users.sample.id,
+    name: Faker::Hipster.sentence,
+    description: Faker::Hipster.paragraph,
+    start_date: Faker::Date.between(2.days.ago, Date.today),
+    end_date: Faker::Date.between(Date.today, 2.days.from_now),
+    location: Faker::Address.city
+
+  )
+end
+
+@trips = Trip.all
+
+10.times do
+  Event.create(
+    trip_id: @trips.sample.id,
+    creator_id: @users.sample.id,
+    name: Faker::Hipster.sentence,
+    description: Faker::Hipster.paragraph,
+    start_time: Faker::Date.between(DateTime.now - 1, DateTime.now),
+    end_time: Faker::Date.between(DateTime.now, DateTime.now + 1),
+    location: Faker::Address.city,
+    private: false
+
   )
 end
