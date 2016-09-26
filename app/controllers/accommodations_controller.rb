34bbd0d1957d2c0cc55730f8be00ef576701e3ad
Nicholas Davidson <ndavidson.sf@me.com>
2016-09-25 17:18:19 -0700

    Adds index route to accommodations controller rendered as json objects

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,4 +1,17 @@
 class AccommodationsController < ApplicationController
   def index
+    @accommodations = Accommodation.all
+
+    @json = @accommodations.map do | accommodation |
+      {
+        id: accommodation.id,
+        creator_id: accommodation.creator_id,
+        trip_id: trip_id,
+        location: location,
+        check_in: check_in,
+        check_out: check_out
+      }
+    end
+    render :json => @json
   end
 end
