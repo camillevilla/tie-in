3ecc050e265804d4c8381f67620eec04f9b50e3c
Camille Villa <cvilla100@gmail.com>
2016-09-26 14:45:44 -0700

    Add and pass tests for Friendships

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+     it { should have_db_column(:user_id) }
+     it { should have_db_column(:friend_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:friend).class_name('User') }
+  end
 end
+
