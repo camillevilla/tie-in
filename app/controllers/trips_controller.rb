30fc9de1c8e88976e60a2bfcab4ba1bf6f2828dd
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-09-26 19:46:03 -0700

    Add trip form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,39 +12,25 @@ class TripsController < ApplicationController
     @trip = Trip.new
   end
 
+  def create
+    @user = current_user
+    @trip = Trip.new(trip_params)
+    @trip.creator_id = @user.id
+    if @trip.save
+      @trip.users << current_user
+      redirect_to @trip
+    else
+      @errors = @trip.errors.full_messages
+      render 'new'
+    end
+  end
+
   def show
     @trip = Trip.find(params[:id])
   end
 
-  def create
-    @current_user = User.first # Need to change this once we have an actual current user!
-    @newTrip = Trip.new(
-        name: params["name"],
-        location: params["location"],
-        description: params["description"],
-        start_date: Date.parse(params["start_date"]),
-        end_date: Date.parse(params["end_date"])
-      )
-
-    @newTrip.creator_id = @current_user.id
-
-    if @newTrip.save
-      # Automatically make user a member of her own trip
-      @newTrip.users << @current_user
-      @current_user.trips << @newTrip
-      render :json =>
-      {
-        status: "ok",
-        message: "Trip saved!",
-        object: @newTrip
-      }.to_json
-    else
-      render :json =>
-      {
-        status: "error",
-        message: @newTrip.errors.full_messages.to_sentence,
-        object: @newTrip
-      }.to_json
-    end
+  private
+  def trip_params
+    params.require(:trip).permit(:name, :location, :description, :start_date, :end_date)
   end
 end
