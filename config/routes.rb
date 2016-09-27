92ded1109e1518b976c7ab47722444de96f7dcba
Camille Villa <cvilla100@gmail.com>
2016-09-27 09:50:08 -0700

    Add Devise User controllers and update model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   get "users/:id/trips" => "trips#index"
 
   # get "trips/new" => "trips#new"
