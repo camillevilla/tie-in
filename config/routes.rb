45ead7fc7f1f3cee052672b3c61e0749402530f6
Camille Villa <cvilla100@gmail.com>
2016-09-27 14:58:36 -0700

    Add Locations controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
 # Enable nested routes, e.g. /trips/1/accommodations
   resources :trips do
-    resources :accommodations, :transits, :events
+    resources :accommodations, :transits, :events, :locations
   end
 
   resources :accommodations
