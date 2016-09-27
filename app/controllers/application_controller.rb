1bfac1b8eab7920bcd95b3bfdeeb4a3f896ba6fa
Camille Villa <cvilla100@gmail.com>
2016-09-26 18:12:38 -0700

    Add Devise to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_paramaters
+    devise_paramater_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
