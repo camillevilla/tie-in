9ade4398c540d9e7e8d96c93ef5c31d87d569c2b
Camille Villa <cvilla100@gmail.com>
2016-09-27 17:22:49 -0700

    Add view for all accommodations on a trip

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,17 +1,65 @@
 class AccommodationsController < ApplicationController
   def index
-    @accommodations = Accommodation.all
+    # /accommodations
+    # @accommodations = Accommodation.all
 
-    @json = @accommodations.map do | accommodation |
-      {
-        id: accommodation.id,
-        creator_id: accommodation.creator_id,
-        trip_id: accommodation.trip_id,
-        location: accommodation.location,
-        check_in: pretty_date(accommodation.check_in),
-        check_out: pretty_date(accommodation.check_out)
-      }
+    # /trips/1/accommodations
+    if params[:trip_id]
+      @trip = Trip.find(params[:trip_id])
+      @accommodations = @trip.accommodations
     end
-    render :json => @json
+
+    # /trips/1/users/1/accommodations
+  end
+
+  def my_accommodations
+    # trips/1/my_accommodations
+    @trip = Trip.find(params[:trip_id])
+    # @accommodations =
+  end
+
+  def show
+     @accommodation = Accommodation.find(params[:id])
+  end
+
+  def new
+    @accommodation = Accommodation.new
+  end
+
+  def edit
+    @accommodation = Accommodation.find(params[:id])
+  end
+
+  def create
+    @accommodation = Accommodation.new(accommodation_params)
+
+    if @accommodation.save
+      redirect_to accommodation_path(@accommodation)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @accommodation = Accommodation.find(params[:id])
+
+    if @accommodation.update(accommodation_params)
+      redirect_to @accommodation
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @accommodation = Accommodation.find(params[:id])
+    @accommodation.destroy
+
+    redirect_to accommodations_path
+  end
+
+  private
+
+  def accommodation_params
+    params.require(:accommodation).permit(:creator_id, :trip_id, :location_id, :check_in, :check_out )
   end
 end
